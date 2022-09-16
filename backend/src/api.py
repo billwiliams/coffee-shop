5615282e42224fdee3efc29b3b8f049ec138710d
Wilson Ndirangu <wnwillyndirangu@gmail.com>
2022-09-16 08:53:12 +0300

    Add Common Errors

--- backend/src/api.py
+++ backend/src/api.py
@@ -91,24 +91,53 @@ def unprocessable(error):
     }), 422
 
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False,
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-
-'''
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above
-'''
+@app.errorhandler(404)
+def not_found(error):
+    return (
+        jsonify({"success": False,
+                "error": 404,
+                 "message": "resource not found"}),
+        404,
+    )
 
 
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above
-'''
+
+@app.errorhandler(400)
+def bad_request(error):
+    return (jsonify({"success": False,
+                    "error": 400,
+                     "message": "bad request"}),
+            400,
+            )
+
+
+@app.errorhandler(405)
+def not_allowed(error):
+    return (
+        jsonify({"success": False,
+                 "error": 405,
+                 "message": "method not allowed"}),
+        405,
+
+    )
+
+@app.errorhandler(401)
+def unauthorized(error):
+    return (
+        jsonify({"success": False,
+                 "error": 401,
+                 "message": "Unauthorized"}),
+        401,
+
+    )
+
+@app.errorhandler(403)
+def forbidden(error):
+    return (
+        jsonify({"success": False,
+                 "error": 403,
+                 "message": "Forbidden"}),
+        403,
+
+    )
+
